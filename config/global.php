613e9b3e9f8ba83f5f47f7efa20cf0181f083a30
Mario Toc <68043864+MarioToc-18@users.noreply.github.com>
2020-10-07 23:57:11 -0600

    Add files via upload

---
+++ config/global.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+
+    'sistema' => 'SISAP',
+    'tema' => 'theme-blue',
+    'organizacion' => 'COMITÉ PRO-INTRODUCCIÓN DE AGUA POTABLE, POR BOMBEO DEL CASERIO CENTRAL ALDEA CHAQUIJYÁ, SOLOLÁ'
+);
